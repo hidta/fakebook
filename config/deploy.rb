7ea60ddac02090e34d875766870b83e3b65368f9
hidta <tahkyu3@outlook.jp>
2017-03-14 14:36:01 +0000

    precompile済みunicorn error修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.0'
 
 set :application, 'fakebook'
-set :repo_url, 'git@github.com:hidta/fakebook.git'
+set :repo_url, 'https://github.com/hidta/fakebook'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -11,25 +11,30 @@ $pid = File.expand_path 'log/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
 #上記設定が適応されるように定義
-worker_processes $worker
+worker_processes  $worker
 working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
 timeout $timeout
-listen $listen
+listen  $listen
 pid $pid
 
 #ホットデプロイ設定
 preload_app true
 
 #fork前に行うこと
-before_fork dl |server, worker|
+before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{sever.config[:pid]}.oldbin"
+  old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
     begin
       Process.kill "QUIT", File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH 
-    end 
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
   end
+end
+
+#fork後に行うこと
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
